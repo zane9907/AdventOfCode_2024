64c5f556546aa2884ed11abefacc4e21703b4e2a
Botond Antalóczy <zane9907@gmail.com>
2024-12-02 11:49:49 +0100

    Add project files.

---
+++ AdventOfCode_02/Program.cs
@@ -0,0 +1,94 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdventOfCode_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzle.txt");
+
+            string[] lines = File.ReadAllLines(path);
+
+            int safe = 0;
+
+            foreach (string line in lines)
+            {
+                List<int> data = line.Split(' ').ToList().ConvertAll(int.Parse);
+
+                bool isSafe = is_safe(data);
+                if (isSafe)
+                {
+                    safe++;
+                }
+            }
+
+            //part 2
+
+            safe = 0;
+            foreach (string line in lines)
+            {
+                List<int> data = line.Split(' ').ToList().ConvertAll(int.Parse);
+
+                //increasing
+                for (int i = 0; i < data.Count; i++)
+                {
+                    List<int> newData = [.. data];
+                    newData.RemoveAt(i);
+                    bool isSafe = is_safe(newData);
+                    if (isSafe)
+                    {
+                        safe++;
+                        break;
+                    }
+                }
+            }
+
+
+
+            ;
+
+        }
+
+        private static bool is_safe(List<int> data)
+        {
+            bool isIncreasing = true;
+            //increasing
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                if (data[i] >= data[i + 1] || !Differ1to3(data[i], data[i + 1]))
+                {
+                    isIncreasing = false;
+                    break;
+                }
+            }
+            if (isIncreasing)
+            {
+                return true;
+            }
+
+            bool isDecreasing = true;
+            //decreasing
+            for (int i = 0; i < data.Count - 1; i++)
+            {
+                if (data[i] <= data[i + 1] || !Differ1to3(data[i], data[i + 1]))
+                {
+                    isDecreasing = false;
+                    break;
+                }
+            }
+            if (isDecreasing)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool Differ1to3(int a, int b)
+        {
+            int result = Math.Abs(a - b);
+            return result >= 1 && result <= 3;
+        }
+    }
+}
---
+++ AdventOfCode_2024/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> left = [];
+            List<int> right = [];
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzle.txt");
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string oneline in lines)
+            {
+                left.Add(int.Parse(oneline.Split(' ')[0]));
+                right.Add(int.Parse(oneline.Split(' ')[1]));
+            }
+
+            int sum = 0;
+
+            while (left.Count > 0)
+            {
+                int leftMin = left.Min();
+                int rightMin = right.Min();
+
+                sum += Math.Abs(leftMin - rightMin);
+                left.Remove(leftMin);
+                right.Remove(rightMin);
+            }
+
+
+            Console.WriteLine(sum);
+
+            //Part 2
+
+            foreach (string oneline in lines)
+            {
+                left.Add(int.Parse(oneline.Split(' ')[0]));
+                right.Add(int.Parse(oneline.Split(' ')[1]));
+            }
+
+            sum = 0;
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                int count = right.Count(x => x == left[i]);
+                sum += left[i] * count;
+            }
+
+
+            Console.WriteLine(sum);
+
+            ;
+        }
+    }
+}
