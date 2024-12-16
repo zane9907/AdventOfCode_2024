26c15820d0a1744f4720de6e2f91e26695c98fb1
Botond Antaloczy <Botond.Antaloczy@dmgmori-heitec.hu>
2024-12-16 15:35:31 +0100

    day 13 part 1 done

--- AdventOfCode_13/Program.cs
+++ AdventOfCode_13/Program.cs
@@ -1,14 +1,54 @@
 ﻿using AdventOfCode_Helper;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode_13
 {
     internal class Program
     {
+        static void test()
+        {
+            long[] totals = new long[2];
+            string[] lines = Helper.ReadFileString(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzle.txt")).Split(Environment.NewLine)
+            .Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            var regexButton = new Regex(@"Button (?:A|B): X\+(\d+), Y\+(\d+)");
+
+            Vector2 Match(Regex regex, string line)
+            {
+                var g = regex.Match(line).Groups;
+                return new(int.Parse(g[1].Value), int.Parse(g[2].Value));
+            }
+
+            for (int j = 0; j < 2; j++)
+                for (int i = 0; i < lines.Length; i += 3)
+                {   // Good ol' algebra
+                    Vector2 a = Match(regexButton, lines[i]);
+                    Vector2 b = Match(regexButton, lines[i + 1]);
+                    var pr = Match(new(@"Prize: X=(\d+), Y=(\d+)"), lines[i + 2]);
+                    long[] p = [(long)pr.X, (long)pr.Y];
+                    p[0] = j == 1 ? /*Part 2*/ p[0] + 10000000000000 : p[0];
+                    p[1] = j == 1 ? /*Part 2*/ p[1] + 10000000000000 : p[1];
+                    double y = (p[1] * (double)a.X - p[0] * (double)a.Y)
+                        / (b.Y * (double)a.X - b.X * (double)a.Y);
+                    double x = (p[0] - y * b.X) / a.X;
+                    if (x % 1 != 0 || y % 1 != 0) continue; // Not an int
+                    totals[j] += (long)x * 3 + (long)y;
+                }
+
+            ;
+        }
+
+
+        static List<Game> games = [];
+        static object LockObject = new object();
+
         static void Main(string[] args)
         {
+
+            //test();
             List<string> data = Helper.ReadFileStringList(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "puzzle.txt"));
 
-            List<Game> games = [];
+            games = [];
             int gameCount = 1;
             for (int i = 0; i < data.Count; i += 4)
             {
@@ -26,142 +66,156 @@ namespace AdventOfCode_13
                     Y = int.Parse(data[i + 1].Split(':')[1].Split(',')[1].Replace(" Y+", ""))
                 };
 
-                int x = int.Parse(data[i + 2].Split(':')[1].Split(',')[0].Replace(" X=", ""));
-                int y = int.Parse(data[i + 2].Split(':')[1].Split(',')[1].Replace(" Y=", ""));
+                long x = int.Parse(data[i + 2].Split(':')[1].Split(',')[0].Replace(" X=", "")) + 10000000000000;
+                long y = int.Parse(data[i + 2].Split(':')[1].Split(',')[1].Replace(" Y=", "")) + 10000000000000;
 
                 games.Add(new()
                 {
                     ID = gameCount++,
+                    ButtonA = buttonA,
+                    ButtonB = buttonB,
                     Buttons = [buttonA, buttonB],
                     Prize = (x, y)
                 });
             }
 
-            int tokens = 0;
-            //foreach (Game game in games)
+            long tokens = 0;
+
+            foreach (var game in games)
+            {
+                Console.WriteLine($"Game {game.ID} started!");
+                long count = CalculateTokens(game);
+                tokens += count;
+                Console.WriteLine($"Game {game.ID} ENDED -------- {count}");
+
+            }
+
+            //Parallel.ForEach(games, game =>
             //{
-            //    Console.WriteLine(gameCount);
-            //    List<Button> pressedButtons = [];
-            //    List<Button> optimal = [];
-            //    bool found = false;
-            //    Backtrack(0, ref pressedButtons, ref optimal, game, ref found);
+            //    Console.WriteLine($"Game {game.ID} started!");
+            //    int count = CalculateTokens(game);
+            //    lock (LockObject)
+            //    {
+            //        tokens += count;
+            //        Console.WriteLine($"Game {game.ID} ENDED");
+            //    }
+            //});
 
-            //    tokens += CalculateTokens(optimal);
-            //    gameCount++;
+
+            ;
+
+
+            //Console.Clear();
+            //for (int i = 0; i < mapX.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < mapX.GetLength(1); j++)
+            //    {
+            //        Console.Write(mapX[i, j] + " ");
+            //    }
+            //    Console.WriteLine();
             //}
-
-            Parallel.ForEach(games, game =>
-            {
-                Console.WriteLine(game.ID); 
-                
-                List<Button> pressedButtons = [];
-                List<Button> optimal = [];
-                bool found = false;
-                Backtrack(0, ref pressedButtons, ref optimal, game, ref found);
-
-                lock (lockobject)
-                {
-                    tokens += CalculateTokens(optimal);
-                    Console.WriteLine();
-                    Console.WriteLine($"Game {game.ID} DONE");
-                }
-            });
-
-
         }
 
-        static object lockobject = new object();
-
-        static void Backtrack(int press, ref List<Button> pressedButtons, ref List<Button> optimal, Game game,ref bool found)
+        static long CalculateTokens(Game game)
         {
-            int i = 0;
-            while (!found && i < 2)
+            long[,] mapX = new long[10001, 10001];
+            for (long i = 0; i < mapX.GetLength(0); i++)
             {
-                if (Ft(pressedButtons))
+                for (long j = 0; j < mapX.GetLength(1); j++)
                 {
-                    if (Fk(press, game, game.Buttons[i], pressedButtons))
+                    //if (i == j)
+                    //{
+                    //    mapX[i, j] = 0;
+                    //    continue;
+                    //}
+
+                    mapX[i, j] = game.ButtonA.X * j + game.ButtonB.X * i;
+                }
+            }
+            
+            List<(long x, long y)> Xindexes = [];
+            for (long i = 0; i < mapX.GetLength(0); i++)
+            {
+                for (long j = 0; j < mapX.GetLength(1); j++)
+                {
+                    if (mapX[i, j] == game.Prize.x)
                     {
-                        if (pressedButtons.Count <= press)
-                        {
-                            pressedButtons.Add(game.Buttons[i]);
-                        }
-                        else
-                        {
-                            pressedButtons[press] = game.Buttons[i];
-                        }
+                        Xindexes.Add((i, j));
+                    }
 
-                        if (pressedButtons.Sum(button => button.X) == game.Prize.x && pressedButtons.Sum(button => button.Y) == game.Prize.y)
-                        {
-                            if (CalculateTokens(pressedButtons) > CalculateTokens(optimal))
-                            {
-                                optimal = new(pressedButtons);
-                                found = true;
-                            }
-                        }
-                        else
-                        {
-                            Backtrack(press + 1, ref pressedButtons, ref optimal, game,ref found);
-                        }
-                    } 
+                    //if (mapX[i, j] > game.Prize.x)
+                    //{
+                    //    j = 0;
+                    //    break;
+                    //}
                 }
-                i++;
             }
 
-            if (press == pressedButtons.Count - 1)
-            {
-                pressedButtons.RemoveAt(press);
-            }
-        }
 
-        static int CalculateTokens(List<Button> pressedButtons)
-        {
-            int tokens = 0;
-            foreach (Button button in pressedButtons)
+            ;
+
+            long[,] mapY = new long[10001, 10001];
+            for (long i = 0; i < mapY.GetLength(0); i++)
             {
-                if (button.Name.Equals('A'))
+                for (long j = 0; j < mapY.GetLength(1); j++)
                 {
-                    tokens += 3;
-                }
-                else
-                {
-                    tokens++;
+                    //if (i == j)
+                    //{
+                    //    mapY[i, j] = 0;
+                    //    continue;
+                    //}
+
+                    mapY[i, j] = game.ButtonA.Y * j + game.ButtonB.Y * i;
                 }
             }
 
-            return tokens;
-        }
-
-        static bool Ft(List<Button> pressedButtons)
-        {
-            return pressedButtons.Count(x => x.Name.Equals('A')) <= 100 && pressedButtons.Count(x => x.Name.Equals('B')) <= 100;
-        }
-
-        static bool Fk(int press, Game game, Button button, List<Button> pressedButtons)
-        {
-            int sumX = 0;
-            int sumY = 0;
-            for (int i = 0; i < press; i++)
+            List<(long x, long y)> Yindexes = [];
+            for (long i = 0; i < mapY.GetLength(0); i++)
             {
-                sumX += pressedButtons[i].X;
-                sumY += pressedButtons[i].Y;
+                for (long j = 0; j < mapY.GetLength(1); j++)
+                {
+                    if (mapY[i, j] == game.Prize.y)
+                    {
+                        Yindexes.Add((i, j));
+                    }
+
+                    //if (mapY[i, j] > game.Prize.y)
+                    //{
+                    //    j = 0;
+                    //    break;
+                    //}
+                }
             }
 
-            return sumX + button.X <= game.Prize.x && sumY + button.Y <= game.Prize.y;
+            (long x, long y) result = (0, 0);
+            foreach (var item in Xindexes)
+            {
+                if (Yindexes.Contains(item))
+                {
+                    result = item;
+                    break;
+                }
+            }
+
+            return 3 * result.y + result.x;
         }
-    }
 
-    public class Button
-    {
-        public char Name { get; set; }
 
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
+        public class Button
+        {
+            public char Name { get; set; }
 
-    public class Game
-    {
-        public int ID { get; set; }
-        public List<Button> Buttons { get; set; }
-        public (int x, int y) Prize { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public class Game
+        {
+            public int ID { get; set; }
+            public Button ButtonA { get; set; }
+            public Button ButtonB { get; set; }
+            public List<Button> Buttons { get; set; } = [];
+            public (long x, long y) Prize { get; set; }
+        }
     }
 }
